c3440631eb36ac33848ae94695cd59e3533031b4
ErikOKriz <xzkire321@gmail.com>
2020-05-21 15:34:43 -0400

    new html page fetcher

---
+++ goFetch.py
@@ -0,0 +1,5 @@
+
+#this program takes a url and retrieves the html file from that url, and stores it in text form
+#in htmlTemp.txt, the file should be rewritten every time thsi program is run.
+def goFetch(url):
+    return
