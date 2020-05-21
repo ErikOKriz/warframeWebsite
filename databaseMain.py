4f9c086d74fef04d30dd55f69e077b9518714245
ErikOKriz <xzkire321@gmail.com>
2020-05-21 15:35:26 -0400

    urlGenerator.py

---
+++ databaseMain.py
@@ -0,0 +1,11 @@
+
+
+#this main function should lok at our primes wanted Doc and for each line
+#       create a url for the item on that line
+#       fetch that url's html file and write to to htmlTemp.txt
+#       parse htmlTemp.txt using another, external function, Parse()
+#       using the info from parse(), write an entry for our file onto our database in the correct format.
+# once it does that for every line in primes wanted, it closes the files and our database should be ready
+
+def main():
+    return
