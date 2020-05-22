959647efe0810b5c53e18f3b275b527f8733a335
ErikOKriz <xzkire321@gmail.com>
2020-05-22 11:39:49 -0400

    psuedocode outlined, still needs helper function

--- databaseMain.py
+++ databaseMain.py
@@ -1,4 +1,6 @@
-
+from urlGenerator import urlCreator
+from goFetch import goFetch
+import dropSearch
 
 #this main function should lok at our primes wanted Doc and for each line
 #       create a url for the item on that line
@@ -8,4 +10,20 @@
 # once it does that for every line in primes wanted, it closes the files and our database should be ready
 
 def main():
+    #primes is the list of items we want
+    primes = open(primes.txt, 'r')
+    #this deletes database for a new one, keep old copies ;)
+    database = open(database.txt, 'w')
+
+    #each line in primes is a different number
+    for line in primes:
+        #use url generator to create the url to search
+        url, name = urlGenerator.urlCreator(line)
+        #get the html of that url
+        goFetch.goFetch(url)
+        #parse out the drop tables from the html
+        dropTables = Parser()
+        #insert the drop tables into database.txt
+    primes.close()
+    database.close()
     return
