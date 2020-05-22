3514a990b7e3a64d373994a4259387534729db67
ErikOKriz <xzkire321@gmail.com>
2020-05-22 11:40:24 -0400

    should be last helper function for databaseMain

---
+++ dropSearch.py
@@ -0,0 +1,3 @@
+#This program will take no input: this function will search
+# through htmlTmp.txt, which contains the html information of a warframe wiki
+# page and returns a formated list of the drop tables for that item
