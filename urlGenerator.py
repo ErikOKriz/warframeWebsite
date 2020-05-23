ca143e363fd0469fb557252add163a7b35a2f974
ErikOKriz <xzkire321@gmail.com>
2020-05-23 13:17:10 -0400

    just some light changes to capitalization of variables

--- urlGenerator.py
+++ urlGenerator.py
@@ -4,18 +4,21 @@
 # case, example: Atlas Prime - warframe
 # https://warframe.fandom.com/wiki/Soma_Prime
 # https://warframe.fandom.com/wiki/Atlas/Prime
+
 def urlCreator(line):
     sep = line.index('-')
     # item name is everything before the hyphen and type is everything after the hyphen
     itemName = line[:sep].strip()
-    type = line[sep + 1:].strip()
+    type = line[sep + 1:].strip().lower()
     # on the warframe wiki, a prime warframe is a sub-page of the warframe ie. wiki.com/atlas/prime
     # while weapons occupy a totally different page ie. wiki.com/Tekko_Prime
     space = itemName.index(' ')
     if type == 'weapon':
-        itemName[space] = '_'
+        typeCode = 1
+        itemName = itemName[:space] + '_' + itemName[space+1:].capitalize()
         url = "https://warframe.fandom.com/wiki/" + itemName
     elif type == 'warframe':
+        typeCode = 2
         # warframes should only be split into two words, warframe name, and Prime
 
         # this next line should make itemName just the warframe's name, because
@@ -27,4 +30,4 @@ def urlCreator(line):
     else:
         return "not a valid type"
 
-    return url, itemName
+    return url, typeCode
