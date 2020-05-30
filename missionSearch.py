5c2919c5ebb4d87d8ac38457a774134d1f5462fb
ErikOKriz <xzkire321@gmail.com>
2020-05-29 23:57:25 -0400

    added a MD function. Ex function works for alot of types

--- missionSearch.py
+++ missionSearch.py
@@ -5,7 +5,13 @@
 # set(capture, defection, defense, disruption, Empyrean, excavation,
 #       extermination, interception, mobile defence, onslaught, rush
 #       sabotage, spy, survival, profit-taker Bounty, salvage, pursuit,
-#
+
+
+#The Capture function works for Capture missions
+#The Defense function works for Defense missions
+#The Exterminate function works for the exterminate,
+# defection, excavation, Survival, Spy, Sabotage, rescue, and Interception pages
+
 
 #Note, if oyu modify the tier system in Capture, it could be repurposed
 # to work for capture and exterminate
@@ -217,7 +223,8 @@ def Defense():
 #test
 #Defense()
 
-
+#also works for Defection, Excavation, Survival, Spy, sabotage, rescue
+# and Interception
 def Exterminate():
     #initialize the file and the lines
     file = open('htmlTemp.txt', 'r')
@@ -300,6 +307,10 @@ def Exterminate():
                 tempList[-1] = tempList[-1][:-1]
                 tempList.append('-')
 
+            #possible that elif is a problem here
+            elif "Tier" in tempList[-1]:
+                tempList[-1] = tempList[-1][-1]
+
             #append a fourple to missionList of all pertinant info.
             missionList.append(tuple(tempList))
 
@@ -307,3 +318,80 @@ def Exterminate():
 
 #testing
 #print(Exterminate())
+
+def Assassinaion():
+    return
+
+def mobileDefense():
+
+    #initialize the file and the lines
+    file = open('htmlTemp.txt', 'r')
+    lines = file.readlines()
+    lines = [x.strip() for x in lines]
+    file.close()
+
+    #filePoint just helps to know where in the file we are
+    filePoint = 0
+
+    #missionList will be our final return value. It will be
+    # a list of tuples, where each tuple, x, will have 4 items
+    # in it, x[0] will be the planet, x[1] will be the mission
+    # name, x[2] will be the faction at the mission,  x[3]
+    # will be the level range of the mission, and x[4] will be
+    # the tier of the mission. This format should be consistent
+    # across all mission types
+    missionList = []
+
+    for line in lines:
+        if filePoint == 0:
+            if "Level" in line:
+                filePoint = 1
+        elif filePoint == 1:
+
+            #exit clause, doesn't matter if there are multiples of
+            # total in the file as this will only trigger on the
+            # 'total' we want.
+            if "total" in line:
+                break
+
+            #setting up place holder variables and iterable list
+            tempList = []
+
+            prev = 0
+
+            listLine = list(line)
+
+            #only the first digit found is important, that signifies the start of
+            # the level range
+            digFound = 0
+
+            for y in range(len(listLine)):
+
+                if listLine[y] == ' ':
+                    listLine[y+1] = 'd'
+
+                #So we don't append empty strings
+                if prev != y:
+                    if listLine[y].isupper():
+                        #this if statement could be more efficient
+                        tempList.append(line[prev:y].strip())
+                        prev = y
+                    elif listLine[y].isdigit() and digFound == 0:
+                        tempList.append(line[prev:y].strip())
+                        prev = y
+                        digFound = 1
+
+            #append what's left in the line, which is the mission level range
+            tempList.append(line[prev:])
+
+            #since none of these missions have tiers
+            tempList.append('-')
+
+            #append a fourple to missionList of all pertinant info.
+            missionList.append(tuple(tempList))
+
+    return missionList
+
+#print(mobileDefense())
+
+
