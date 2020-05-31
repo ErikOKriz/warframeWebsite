a3d2203b87c3c0513870ab7f1a4e994fafd5ccd4
Luca Letizia <qdog912@gmail.com>
2020-05-30 22:03:04 -0700

    database.json updated
    2 more entries added to database.json

--- primeScript.js
+++ primeScript.js
@@ -1,8 +1,28 @@
-var request = new XMLHttpRequest();
-request.open('GET','https://raw.githubusercontent.com/ErikOKriz/warframeWebsite/Luca/primes.json');
-request.onload = function(){
-    var primeList = JSON.parse(request.responseText);
-    //var primeList = request.responseText;
-    console.log(primeList[0])
+//Define containers for use later
+var primeList = document.getElementById("primeList");
+var primes;
+var primeCt;
+
+//Funcs
+function addPrime(tmp){
+    primeList.insertAdjacentHTML('beforeend', `<p>` + tmp.name + `</p>\n`);
 }
+
+//Fetch database.json
+var request = new XMLHttpRequest();
+request.open('GET','https://raw.githubusercontent.com/ErikOKriz/warframeWebsite/Luca/database.json');
+request.onload = function(){
+    primes = JSON.parse(request.responseText);
+    primeCt = primes.length;
+    console.log(primeCt);                           //Debug Print
+
+    //Populate primelist with each prime
+    for(var i = 0; i < primeCt; i++){
+        addPrime(primes[i]);
+    }
+
+};
 request.send();
+
+
+
