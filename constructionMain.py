fcf1860d1d00e4707f8b3ae4325b221ddeca1912
ErikOKriz <xzkire321@gmail.com>
2020-06-03 20:03:04 -0400

    created main database creator function

---
+++ constructionMain.py
@@ -0,0 +1,10 @@
+import primeParser
+import relicParse
+
+
+#main function which reconstructs database if the need arises
+def main():
+    primeParser.primeBaseMain()
+    relicParse.relicParseMain()
+
+
