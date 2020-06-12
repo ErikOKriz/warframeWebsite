87170b5f2f16260b0ab91f0fdb64c0a80ca5f8e9
ErikOKriz <xzkire321@gmail.com>
2020-06-11 21:48:50 -0400

    inefficient but ready for work

--- relicParse.py
+++ relicParse.py
@@ -7,6 +7,9 @@ import json
 # file that contains all that info.
 def relicParse():
 
+    #place what we need in htmlTemp.txt
+    goFetch("https://warframe.fandom.com/wiki/Void_Relic")
+
     # initialize the file and the lines
     file = open('htmlTemp.txt', 'r')
     lines = file.readlines()
@@ -19,9 +22,6 @@ def relicParse():
     #this will be our final return list, it'll be a list of objects described by
     # tempList
     relicList = []
-
-    #list of all vaulted relics
-    vaultedList = set()
 
     #this will be all the info about a relic. it will be a list containing the name of
     # the relic, the list of the drop table from the relic, with the first three listed
@@ -40,30 +40,22 @@ def relicParse():
         elif filePoint == 1:
             #start inserting info into tempList
             if "Lith" in line or "Meso" in line or "Neo" in line or "Axi" in line:
-                tempList = [line, [], []]
+                tempList = [line, []]
                 filePoint = 2
 
         elif filePoint == 2:
-            tempList[1].append(line)
-            if len(tempList[1]) == 6:
+            if "Chance" in line:
                 filePoint = 3
 
         elif filePoint == 3:
-            if "Chance" in line:
-                filePoint = 4
-
-        elif filePoint == 4:
             if "Lith" in line or "Meso" in line or "Neo" in line or "Axi" in line:
                 relicList.append(tempList)
-                tempList = [line, [], []]
+                tempList = [line, []]
                 filePoint = 2
                 continue
 
-            #final break
             elif "For information" in line:
-                filePoint = 5
-                continue
-
+                break
             #implied else
 
             #The rest of this goes on to construct tempList[2], which is a list of all
@@ -83,60 +75,116 @@ def relicParse():
                     prev = y
             newLine.append(line[prev:prev + 1])
             newLine.append(line[prev + 1:])
-            tempList[2].append(newLine)
+            tempList[1].append(newLine)
 
+    return relicList
 
-        elif filePoint == 5:
-            if "Axi" in line:
-                filePoint = 6
+def VRelicParse():
 
-        elif filePoint == 6:
-            if "Baro" in line:
+    #place what we need in htmlTemp.txt
+    goFetch("https://warframe.fandom.com/wiki/Module:Void/data?action=edit")
+
+    # initialize the file and the lines
+    file = open('htmlTemp.txt', 'r')
+    lines = file.readlines()
+    lines = [x.strip() for x in lines]
+    file.close()
+
+    # filePoint just helps to know where in the file we are
+    filePoint = 0
+
+    newLine = ''
+
+    for line in lines:
+        if filePoint == 0:
+            if '"Relics"' in line:
+                filePoint = 1
+        elif filePoint == 1:
+            if line.strip() == '}':
                 break
-            else:
-                vaultedList.add(line)
+            #implied else
+            newLine = newLine + line
 
-    return relicList, vaultedList
+    #these replace effects are what turns our string into one that is accepted by json.loads(x)
+    #can definately be made more efficient, but this works for now
+    newLine = newLine.replace("Drops = { {", '"Drops" : [ [')
+    newLine = newLine.replace('}}', ']]')
+    newLine = newLine.replace(' = '," : ")
+    newLine = newLine.replace('Item', '"Item"')
+    newLine = newLine.replace("Part", '"Part"')
+    newLine = newLine.replace("Rarity", '"Rarity"')
+    newLine = newLine.replace("Tier", '"Tier"')
+    newLine = newLine.replace("Name", '"Name"')
+    newLine = newLine.replace("IsVaulted", '"IsVaulted"')
+    newLine = newLine.replace('IsBaro', '"IsBaro"')
+    newLine = newLine.replace('},{"Item" : ', '],[')
+    newLine = newLine.replace('"Item" : ', '')
+    newLine = newLine.replace('"Part" : ', '')
+    newLine = newLine.replace('"Rarity" :', '')
+    #this line gives us a good character to split the relic dicts by "?"
+    newLine = newLine.replace('},{ "Tier"', '}?{"Tier"')
 
+    #just to check what is getting outputed, for testing
+    #with open('htmlTemp2.txt', 'w') as file:
+        #file.write(newLine)
 
+    relicDicts = []
+
+    #each x at this point will be a dict in json format and cna be worked on.
+    for x in newLine.split('?'):
+        x = json.loads(x)
+        relicDicts.append(x)
+
+    #now have to take this relic Dicts, and use the dropsFrom tables from relicParse
+    # in order to create a complete relic table
+
+    MissionList = relicParse()
+    for Y in relicDicts:
+        Yname = str(Y["Tier"]) + ' ' + str(Y["Name"])
+        for M in MissionList:
+            #testing
+            #print(str(Y["Tier"]) + ' ' + str(Y["Name"]))
+            if M[0] == Yname:
+                #if there is a mission which drops the relic, then it is not vaulted
+                Y['DropsFrom'] = M[1]
+                #once per missionList loop
+                del Y['IsVaulted']
+                MissionList.remove(M)
+                #hopefully this just breaks the missionList loop
+                break
+        if 'DropsFrom' not in Y:
+            Y["DropsFrom"] = '0'
+            if "IsVaulted" in Y:
+                del Y["IsVaulted"]
+        if "IsBaro" not in Y:
+            Y["IsBaro"] = 0
+
+    #after this for loop, relicDicts should be modified in place enough for us
+        #to make it the final return value
+
+    return relicDicts
 
 def relicParseMain():
-    #main relic page
-    url = "https://warframe.fandom.com/wiki/Void_Relic"
-
-    #put the relic page into htmlTemp.txt
-    goFetch(url)
-
-    #parse htmlTemp.txt
-    RList, VList = relicParse()
-
+    #create dictionary objects for all relics
+    dataList = VRelicParse()
     #now create the file relicBase.txt which holds all the information in json format
     data = {}
     data['relics'] = []
 
     iDCount = 0
 
-    for y in range(len(RList)):
-
-        if RList[y][0] in VList:
-            data['relics'].append({
-                'name':RList[y][0],
-                'ID':str(iDCount),
-                'drop table':tuple(RList[y][1]),
-                'mission drops': "Vaulted, Nowhere"
-            })
-            iDCount += 1
-
-        else:
-            data['relics'].append({
-                'name':RList[y][0],
-                'ID':str(iDCount),
-                'drop table':tuple(RList[y][1]),
-                'mission drops':tuple(RList[y][2])
-            })
-            iDCount += 1
+    for y in range(len(dataList)):
+        #test
+        dataList[y]["ID"] = iDCount
+        data['relics'].append(dataList[y])
+        iDCount += 1
 
     with open('relicTables.txt', 'w') as file:
         json.dump(data, file)
 
+
+#for testing
+relicParseMain()
+#VRelicParse()
+#relicParse()
 
