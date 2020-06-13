cd89a85b679b9b0c40adaf8438f72fa84a69043c
Luca Letizia <qdog912@gmail.com>
2020-06-12 18:23:28 -0700

    Relic Info Bar Created (not fully formatted)

--- primeScript.js
+++ primeScript.js
@@ -6,9 +6,10 @@ var primeCt;
     //Database.json and its quantity
 var pTables;
 var pTableCt;
-    //relicMasterTemp.txt and qty
-var rMaster;
-var rMasterCt;
+    //relicList.txt and qty
+var relics;
+var relicCt;
+var relicEraIndex = Array(4);
 
 
 /********* 
@@ -66,7 +67,10 @@ function setFeaturedPrime(ID){
     }
     //Take the name insert _ for spaces if necessary
     var tag = tmp.name.replace(' ', '_');
-    
+
+    //Remove the mid placeholder
+    document.getElementById('itemPlaceholder').innerHTML = "";
+
     //Set Title and Type
     document.getElementById('itemName').innerHTML = tmp.name;
     document.getElementById('itemType').innerHTML = tmp.type;
@@ -76,12 +80,12 @@ function setFeaturedPrime(ID){
     for(var i = 0; i < 4; i++){
         var relicList = [tmp.parts.part0, tmp.parts.part1, tmp.parts.part2, tmp.parts.part3];
         if(i == 3 && relicList[3] == undefined) break;
-        document.getElementById('itemTable').insertAdjacentHTML('beforeend','<tr>\n<th>' + tmp.partNames[i] + '</th>\n');
+        document.getElementById('itemTable').insertAdjacentHTML('beforeend','<tr><th>' + tmp.partNames[i] + '</th>');
         var j = 0;
         while(relicList[i][j] != undefined)
-            document.getElementById('itemTable').insertAdjacentHTML('beforeend','<td>' + relicList[i][j++] + '</td>\n');
+            document.getElementById('itemTable').insertAdjacentHTML('beforeend','<td><a href="#" onclick="javascript:setFeaturedRelic(`' + relicList[i][j] + '`)">' + relicList[i][j++] + '</a></td>');
     }
-    document.getElementById('itemTable').insertAdjacentHTML('beforeend','</tr>\n');
+    document.getElementById('itemTable').insertAdjacentHTML('beforeend','</tr>');
     
     //Set wishlist tickbox. If cookied, display checked. Otherwise, display empty.
     if(getCookie(tag) != null){
@@ -94,8 +98,64 @@ function setFeaturedPrime(ID){
 
     }
 }
+function setFeaturedRelic(droplocation){
+    var dropsplit = droplocation.split(' ');
+    var tier = dropsplit[0];
+    var name = dropsplit[1];
+    var i = 0;
+    var end;
+    
+    //Skip to index that matches tier
+    switch(tier){
+        case "Lith": end =  relicEraIndex[1]; break;
+        case "Meso": i = relicEraIndex[1]; end = relicEraIndex[2]; break;
+        case "Neo": i = relicEraIndex[2]; end = relicEraIndex[3]; break;
+        case "Axi": i = relicEraIndex[3]; end = relicCt; break;
+    }
+    
+    //Find the matching name. If at any point tier != the correct one, 
+    for(i; i < end; i++){
+        if(relics[i].Name == name)
+            break;
+    }
+
+    //Print Contents
+    document.getElementById("relicTitle").innerHTML = relics[i].Tier + " " + relics[i].Name;
+    var relicInfoHTML = document.getElementById("relicInfo");
+    relicInfoHTML.innerHTML = "<h3>Contents</h3>";
+    for(var j = 0; j < 6; j++){
+        var str = relics[i].Drops[j][0];
+        var tmpStr = relics[i].Drops[j][1];
+        str += " " + tmpStr;
+        var rarityStr;
+        switch(j){
+            case 0:
+            case 1:
+            case 2: rarityStr = '<p class="common">'; break;
+            case 3:
+            case 4: rarityStr = '<p class="uncommon">'; break;
+            case 5: rarityStr = '<p class="rare">'; break;
+        }
+        relicInfoHTML.insertAdjacentHTML('beforeend',rarityStr +  str + "</p>");
+    }
+
+    //Print Drop location
+    relicInfoHTML.insertAdjacentHTML('beforeend', "<h3>Drop Locations</h3>");
+    if(relics[i].DropsFrom == "0"){
+        relicInfoHTML.insertAdjacentHTML('beforeend', "<p>This relic is vaulted. :(</p>");
+    }
+    else{
+        var j = 0;
+        while(relics[i].DropsFrom[j] != undefined){
+            var curr = relics[i].DropsFrom[j++];
+            relicInfoHTML.insertAdjacentHTML('beforeend', "<p>" + curr[1] + " " + curr[0] + "</p>");
+        }
+    }
+
+
+}
 function addPrime(tmp){
-    var string = `<a href="#" id="` + tmp.ID + `"onclick="javascript:setFeaturedPrime('` + tmp.ID + `')">` + tmp.name + `</a>\n`;
+    var string = `<a href="#" id="` + tmp.ID + `"onclick="javascript:setFeaturedPrime('` + tmp.ID + `')">` + tmp.name + `</a>`;
     primeList.insertAdjacentHTML('beforeend', string);
 }
 function addWishlist(name){
@@ -154,9 +214,20 @@ request2.send();
 var request3 = new XMLHttpRequest();
 request3.open('GET','https://raw.githubusercontent.com/ErikOKriz/warframeWebsite/master/erikScripts/relicTables.txt');
 request3.onload = function(){
-    rMaster = JSON.parse(request3.responseText);
-    rMasterCt = rMaster.length;
-    console.log(rMaster);
+    relics = JSON.parse(request3.responseText).relics;
+    relicCt = relics.length;
+    //Fetch the era indices
+    relicEraIndex[0] = 0;
+    var i = 0;
+    //Skip to the first meso, then store
+    while(relics[i].Tier != "Meso" || relics[i] == undefined ) i++;
+    relicEraIndex[1] = i;
+    //Repeat for neo and axi
+    while(relics[i].Tier != "Neo" || relics[i] == undefined ) i++;
+    relicEraIndex[2] = i;
+    while(relics[i].Tier != "Axi" || relics[i] == undefined ) i++;
+    relicEraIndex[3] = i;
+    console.log(relics);
 
 }
 request3.send();
