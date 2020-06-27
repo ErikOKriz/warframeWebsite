02882548b3e5480469686406eb6a3f00ba12de08
ErikOKriz <xzkire321@gmail.com>
2020-06-27 16:50:44 -0400

    set up fullsearch to be first to work with later relicParse

--- erikScripts/constructionMain.py
+++ erikScripts/constructionMain.py
@@ -5,13 +5,13 @@ from FullSearch import fullSearchMain
 #main function which reconstructs all databases if the need arises
 def main():
     #need to do relic main first as the prime main pulls from the relic data
+    fullSearchMain()
     relicParseMain()
     primeBaseMain()
-    fullSearchMain()
 
 
 #this function should only call itself for manuel testing. the goal is for another
 # function to automatically call this when the database is out of date
-#main()
+main()
 
 
